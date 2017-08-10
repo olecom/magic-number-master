d15533f39e3af7d0893ac271c53df689b8d33e5d
Oleg Verych <ole4at@gmail.com>
2017-08-10 17:49:50 +0300

    add `getMagicNumberSqrt` && tests: return the square root of a magic number to an asynchronous callback function
    magic number is requested from
    ```
    BASE_URL = process.env.BASE_URL || 'http://apps.wavana.com/magicnumber'
    ```

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 /**
  * This is the module
  */
+
+const BASE_URL = process.env.BASE_URL || 'http://apps.wavana.com/magicnumber'
+const web = require('request-promise-native')
+
+/*
+ * return the square root of a magic number to an asynchronous callback function
+ */
+async function getMagicNumberSqrt(x, y, callback) {
+  const magicNumber = await web({
+    headers: {
+      'User-Agent': 'Node.JS/8'
+    },
+    method: 'get', json: true,
+    url: `${BASE_URL}?x=${+x}&y=${+y}`
+  })
+
+  return setImmediate(() => callback(null, Math.sqrt(magicNumber)))
+}
+
+function callbackHandler(err, marigNumSqrt){
+  console.log('the square root of a magic number:', marigNumSqrt)
+}
+
+// export module interface
+module.exports = getMagicNumberSqrt
+
+// self running example
+if (!module.parent){
+  Promise.all([getMagicNumberSqrt(1, 2, callbackHandler)])
+    .then(() => console.log('Launch of `getMagicNumberSqrt`: OK'))
+    .catch((ex) => console.error('!Error', ex)
+  )
+}
--- test.js
+++ test.js
@@ -1,3 +1,47 @@
 /**
  * This is the test
  */
+
+var test = require('tape');
+var nock = require('nock');
+
+const BASE_URL_FQDN = 'http://apps.wavana.com'
+const BASE_URL_URI = /magicnumber.*/
+
+// disable anything to go out the tests
+test('Before...', (t) => {
+  nock.disableNetConnect()
+  t.end()
+})
+
+// test the module code
+test('getMagicNumberSqrt: requesting magic number from URL and returning sqrt of it', async (t) => {
+  var getMagicNumberSqrt = require('./index.js')
+
+  nock(BASE_URL_FQDN)
+    .get(BASE_URL_URI)
+    .reply(200, 123456789)
+
+  getMagicNumberSqrt(1, 2, (err, magicSqrt) => {
+    t.equal(magicSqrt, Math.sqrt(123456789));
+    t.end();
+  })
+});
+
+test('getMagicNumberSqrt: again with 12345 as magic number', async (t) => {
+  var getMagicNumberSqrt = require('./index.js')
+
+  nock(BASE_URL_FQDN)
+    .get(BASE_URL_URI)
+    .reply(200, 12345)
+
+  getMagicNumberSqrt(1, 2, (err, magicSqrt) => {
+    t.equal(magicSqrt, Math.sqrt(12345));
+    t.end();
+  })
+});
+
+test('...after.', (t) => {
+  nock.enableNetConnect()
+  t.end()
+})
